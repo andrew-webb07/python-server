6f7aff3ab6eb9a5c36aefa9ce0908dbe4efa997f
Andrew Webb <webba@ihopu.org>
2021-07-12 17:08:26 -0500

    added edit ability with PUT function

--- employees/request.py
+++ employees/request.py
@@ -73,3 +73,9 @@ def delete_employee(id):
     # If the employee was found, use pop(int) to remove it from list
     if employee_index >= 0:
         EMPLOYEES.pop(employee_index)
+
+def update_employee(id, new_employee):
+    for index, employee in enumerate(EMPLOYEES):
+        if employee["id"] == id:
+            EMPLOYEES[index] = new_employee
+            break

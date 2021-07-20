5976a8ece58816a0a2ba74d31528babab8b9fb35
Andrew Webb <webba@ihopu.org>
2021-07-20 15:07:46 -0500

    created query searches for animals, customers and employees

--- kennel.sql
+++ kennel.sql
@@ -9,7 +9,7 @@ CREATE TABLE `Customer` (
     `name`    TEXT NOT NULL,
     `address`    TEXT NOT NULL,
     `email`    TEXT NOT NULL,
-    `password`    TEXT NOT NULL
+	`password` TEXT NOT NULL
 );
 
 CREATE TABLE `Animal` (
@@ -58,13 +58,13 @@ INSERT INTO `Animal` VALUES (null, "Doodles", "Kennel", "Poodle", 3, 1);
 INSERT INTO `Animal` VALUES (null, "Daps", "Kennel", "Boxer", 2, 2);
 
 
--- Get only the animal rows where the `id` field value is 3
-SELECT
-    a.id,
-    a.name,
-    a.breed,
-    a.status,
-    a.location_id,
-    a.customer_id
-FROM animal a
-WHERE a.id = 2
+-- -- Get only the animal rows where the `id` field value is 3
+-- SELECT
+--     a.id,
+--     a.name,
+--     a.breed,
+--     a.status,
+--     a.location_id,
+--     a.customer_id
+-- FROM animal a
+-- WHERE a.id = 2
